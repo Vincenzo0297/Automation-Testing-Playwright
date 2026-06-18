9ff05f15990fe77aef86e9793d83bd76161560af
vincent <vincenthaha1997@gmail.com>
2026-06-18 11:21:35 +0800

    update

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -3,29 +3,27 @@ import { test, expect } from '@playwright/test';
 test('Practice Test Automation - Login', async ({ page }) => {
 
   // await page means that we are using the page object to interact with the web page.
-  await page.goto('https://practicetestautomation.com/practice-test-login/');
-
-  // Take screenshot
+  await page.goto('https://vb-bank-demo.vercel.app/login');
+  await page.waitForTimeout(2000);
   await page.screenshot({
-    path: 'screenshots/practice-test-login.png'
+    path: 'screenshots/Mock-Bank-Page.png'
   });
 
-  await page.locator("//*[@id='username']").fill('mynameis');
-  await page.waitForTimeout(5000); 
+  // Click on User button
+  await page.locator('//*[@id="root"]/div/div[1]/div[2]/div/button[1]').click();
+  await page.waitForTimeout(2000);
   await page.screenshot({
-    path: 'screenshots/practice-test-login-username.png'
-  });
-  
-  await page.locator("//*[@id='password']").fill('Password123');
-  await page.waitForTimeout(5000); 
-  await page.screenshot({
-    path: 'screenshots/practice-test-login-password.png'
+    path: 'screenshots/Mock-Bank-User.png'
   });
 
-  
-  await page.locator("//*[@id='submit']").click();
-  await page.waitForTimeout(5000); // 5 seconds
-  await page.screenshot({
-    path: 'screenshots/practice-test-login.png'
+   // verify the total balance is $15,000.00
+   const totalBalance = await page.locator('//*[@id="root"]/div/div/main/div/div/div[2]/div[1]/div[1]/h2').textContent();
+   //the number will be different based on the user, so we will just check if it contains a $ sign
+   expect(totalBalance).toContain('$');
+   await page.waitForTimeout(2000);
+   await page.screenshot({
+    path: 'screenshots/Mock-Bank-Balance.png'
   });
+  console.log(totalBalance);
+
 });
